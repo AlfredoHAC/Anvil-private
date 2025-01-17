04c5d1470af76da33f8e99e658bd8ac2e39845d7
AlfredoHAC <alfredohac@gmail.com>
2025-01-17 11:36:29 -0300

    Add basic platform layer

---
+++ src/Platform/platform.h
@@ -0,0 +1,11 @@
+#ifndef PLATFORM_HEADER
+#define PLATFORM_HEADER
+
+#include "Core/typedefs.h"
+
+#include <stdbool.h>
+
+bool PlatformInit(const char* window_title, uint16 window_width, uint16 window_height);
+
+
+#endif // !PLATFORM_HEADER
