1c572d3d36031698fa63ac780d8cbdfa99e9fd68
AlfredoHAC <alfredohac@gmail.com>
2026-06-25 19:24:57 -0300

    Rename Core/base.h to Platform/platform_detection.h

--- src/anvlpch.h
+++ src/anvlpch.h
@@ -2,7 +2,7 @@
 #define ANVL_PRECOMPILED_HEADER
 // IWYU pragma: begin_exports
 
-#include "Core/base.h"
+#include "Platform/platform_detection.h"
 #include "Platform/typedefs.h"
 
 #include "Tools/logger.h"
