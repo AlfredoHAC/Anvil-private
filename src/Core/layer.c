f0b7e719d54e913d24966e574d8324bd4a62373a
AlfredoHAC <alfredohac@gmail.com>
2026-07-30 20:46:14 -0300

    Add layer stack for managing application layers.

---
+++ src/Core/layer.c
@@ -0,0 +1,96 @@
+#include "anvlpch.h"
+
+#include "Core/layer.h"
+
+#define LAYER_STACK_MAX_LENGTH 32
+
+static Layer* layer_stack[LAYER_STACK_MAX_LENGTH];
+static uint32 layer_stack_length = 0;
+
+void anvl_layer_stack_push(Layer* layer)
+{
+    if (!layer)
+    {
+        ANVIL_CORE_ERROR("Layer invalid.");
+        return;
+    }
+
+    if (layer_stack_length + 1 > LAYER_STACK_MAX_LENGTH)
+    {
+        ANVIL_CORE_ERROR("Layer stack out of space.");
+        return;
+    }
+
+    layer_stack[layer_stack_length] = layer;
+    layer_stack_length += 1;
+}
+
+void anvl_layer_stack_pop()
+{
+    if (layer_stack_length == 0) { return; }
+
+    layer_stack[layer_stack_length] = NULL;
+    layer_stack_length -= 1;
+}
+
+void anvl_layer_stack_remove(Layer* layer)
+{
+    if (!layer)
+    {
+        ANVIL_CORE_ERROR("Layer invalid.");
+        return;
+    }
+
+    if (layer_stack_length == 0) { return; }
+
+    int8 layer_index = 0;
+    for (int8 i = (int8)layer_stack_length - 1; i >= 0; ++i)
+    {
+        if (layer_stack[i] == layer)
+        {
+            layer_index = i;
+            break;
+        }
+    }
+
+    for (int8 i = layer_index; i < layer_stack_length; ++i)
+    {
+        layer_stack[i] = layer_stack[i + 1];
+    }
+
+    layer_stack_length -= 1;
+}
+
+// clang-format off
+uint32 anvl_layer_stack_length()
+{
+    return layer_stack_length;
+}
+
+void anvl_layer_stack_clear()
+{
+    layer_stack_length = 0;
+}
+// clang-format on
+
+void anvl_layer_stack_dispatch_event(Event event)
+{
+    for (int8 i = layer_stack_length - 1; i >= 0; ++i)
+    {
+        if (layer_stack[i] == NULL || layer_stack[i]->on_event == NULL) { continue; }
+
+        layer_stack[i]->on_event(layer_stack[i], event);
+
+        if (event.handled) { break; }
+    }
+}
+
+void anvl_layer_stack_call_update()
+{
+    for (int8 i = layer_stack_length - 1; i >= 0; ++i)
+    {
+        if (layer_stack[i] == NULL || layer_stack[i]->on_update == NULL) { continue; }
+
+        layer_stack[i]->on_update(layer_stack[i]);
+    }
+}
---
+++ src/Core/layer.h
@@ -0,0 +1,26 @@
+#ifndef ANVIL_LAYER_HEADER
+#define ANVIL_LAYER_HEADER
+
+#include "Windowing/event.h"
+
+typedef struct Layer Layer;
+
+typedef void (*LayerOnUpdateFn)(Layer* layer);
+typedef void (*LayerOnEventFn)(Layer* layer, Event event);
+
+struct Layer
+{
+    const char*     name;
+    LayerOnUpdateFn on_update;
+    LayerOnEventFn  on_event;
+};
+
+void   anvl_layer_stack_push(Layer* layer);
+void   anvl_layer_stack_pop();
+void   anvl_layer_stack_remove(Layer* layer);
+uint32 anvl_layer_stack_length();
+void   anvl_layer_stack_clear();
+void   anvl_layer_stack_dispatch_event(Event event);
+void   anvl_layer_stack_call_update();
+
+#endif // ANVIL_LAYER_HEADER
