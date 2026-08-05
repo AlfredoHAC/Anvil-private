b2424376deacec4106d6cd6f3dc3c0087671b55d
AlfredoHAC <alfredohac@gmail.com>
2026-08-05 17:43:54 -0300

    Remove event handling from application

--- src/Core/application.c
+++ src/Core/application.c
@@ -73,48 +73,12 @@ void anvl_application_shutdown(Application* app)
 
 static void _on_application_event(Layer* layer, Event* event)
 {
-    ANVIL_CORE_DEBUG("Event captured!");
-
-    switch (event->type)
+    if (event->type == ANVL_EVENT_TYPE_WINDOW_CLOSE)
     {
-        case ANVL_EVENT_TYPE_WINDOW_CLOSE: _on_application_window_close(); break;
-        case ANVL_EVENT_TYPE_WINDOW_RESIZE:
-            ANVIL_CORE_DEBUG(
-                "Window resize: %dx%d", event->window_resize.width, event->window_resize.height);
-            break;
-        case ANVL_EVENT_TYPE_KEY_PRESS:
-            ANVIL_CORE_DEBUG(
-                "Key press: %d (Mod: %d)", event->key_press.key_code, event->key_press.modifier_set);
-            break;
-        case ANVL_EVENT_TYPE_KEY_RELEASE:
-            ANVIL_CORE_DEBUG("Key release: %d (Mod: %d)",
-                             event->key_release.key_code,
-                             event->key_release.modifier_set);
-            break;
-        case ANVL_EVENT_TYPE_MOUSE_MOVE:
-            ANVIL_CORE_DEBUG("Mouse move: (%.1f,%.1f)", event->mouse_move.x, event->mouse_move.y);
-            break;
-        case ANVL_EVENT_TYPE_MOUSE_BUTTON_CLICK:
-            ANVIL_CORE_DEBUG("Mouse button click: %d (%.1f,%.1f)",
-                             event->mouse_button_click.button_code,
-                             event->mouse_button_click.x,
-                             event->mouse_button_click.y);
-            break;
-        case ANVL_EVENT_TYPE_MOUSE_BUTTON_RELEASE:
-            ANVIL_CORE_DEBUG("Mouse button release: %d (%.1f,%.1f)",
-                             event->mouse_button_release.button_code,
-                             event->mouse_button_release.x,
-                             event->mouse_button_release.y);
-            break;
-        case ANVL_EVENT_TYPE_MOUSE_SCROLL:
-            ANVIL_CORE_DEBUG("Mouse scroll: (%.1f,%.1f)",
-                             event->mouse_scroll.x_offset,
-                             event->mouse_scroll.y_offset);
-            break;
-        default: break;
-    }
+        _on_application_window_close();
 
-    event->handled = true;
+        event->handled = true;
+    }
 }
 
 // clang-format off
