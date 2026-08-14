47887e10a6a34ed15cddd3f809b2fd3431ed33df
AlfredoHAC <alfredohac@gmail.com>
2026-08-14 15:01:23 -0300

    Add WGL graphics context implementation for Windows

--- include/Anvil/Graphics/context.h
+++ include/Anvil/Graphics/context.h
@@ -1,7 +1,7 @@
 #ifndef ANVIL_GRAPHICS_CONTEXT_HEADER
 #define ANVIL_GRAPHICS_CONTEXT_HEADER
 
-#include "Graphics/requirements.h"
+#include "Anvil/Graphics/requirements.h"
 
 typedef struct AnvlGraphicsContext AnvlGraphicsContext;
 
@@ -10,6 +10,7 @@ AnvlGraphicsContext* anvl_graphics_context_create(
     const AnvlGraphicRequirements requirements);
 void anvl_graphics_context_destroy(AnvlGraphicsContext* context);
 
-void* anvl_graphics_context_get_handle(const AnvlGraphicsContext* context);
+const void* anvl_graphics_context_get_handle(
+    const AnvlGraphicsContext* context);
 
 #endif // !ANVIL_GRAPHICS_CONTEXT_HEADER
